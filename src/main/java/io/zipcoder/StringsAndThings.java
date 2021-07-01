72f69da6fc6c26ba975af79268fea3546e518924
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-01 16:00:51 -0400

    count yz tests passed

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,12 @@
 package io.zipcoder;
 
 
+import com.sun.tools.javac.code.Attribute;
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.lang.reflect.Array;
+import java.util.Locale;
+
 /**
  * @author tariq
  */
@@ -14,11 +20,22 @@ public class StringsAndThings {
      *           countYZ("day fez"); // Should return 2
      *           countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
+    public Integer countYZ(String input) {
+        int count = 0;
+                /* String noSym = input.replaceAll("[^a-zA-Z]", ""); */
+        String[] wordArray = input.toLowerCase().split(" ");
+        for (int i = 0; i < wordArray.length; i++) {
+            String currentWord = wordArray[i];
+            boolean endsWithY = currentWord.endsWith("y");
+            boolean endsWithZ = currentWord.endsWith("z");
+            boolean endsWithYOrZ = endsWithZ || endsWithY;
+            if (endsWithYOrZ) {
+                count++;
+            }
+        }
+        return count;
     }
-
-    /**
+       /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
